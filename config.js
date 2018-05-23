ccf8ede86eb9a47db96816e0ba25bd658b37904e
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-05-23 17:53:01 +0300

    port changed

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config.databaseUrl = `mongodb://${dbUserName}:${dbPassword}@ds215208.mlab.com:15
 // config.databaseUrl = "mongodb://user:user@ds215208.mlab.com:15208/blogauth";
 
 config.jwtSecret = "kpTxN=)7mX3W3SEJ58Ubt8-";
-config.port = process.env.port || 8001;
+config.port = process.env.port;// || 8001;
 config.ip = process.env.ip;
 config.userId;
 
